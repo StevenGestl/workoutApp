965e680d32e827d7b69d4c46ad0e3e648dfdfaa0
Steven Gestl <105674896+StevenGestl@users.noreply.github.com>
2024-02-23 06:10:50 -0500

    Update workouts.js

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,7 +1,38 @@
 const express = require('express')
 
-const route = express.Router()
+const router = express.Router()
 
-Router.get('/', () => {} )
+
+// GET all workouts from api
+router.get('/', (req, res) => {
+    res.json({mssg: 'GET all workouts'})
+} )
+
+// GET a single workout from api
+
+router.get('/:id', (res, req) => {
+    res.json({mssg: 'Get a single workout'})
+})
+
+// POST a new workout
+
+router.post('/', (req, res) => {
+    res.json({mssg: 'POST a new workout'})
+})
+
+// DELETE a workout
+
+router.delete('/', (req, res) => {
+    res.json({mssg: 'DELETE a new workout'})
+})
+
+//UPDATE a workout
+
+router.patch('/', (req, res) => {
+    res.json({mssg: 'UPDATE a new workout'})
+})
 
 module.exports = router
+
+
+// nodemon server.js to run 
